122a69ab87681c3448493953c7c66a1d45dd004e
JenniferR326 <jennarafael@gmail.com>
2021-03-09 22:22:57 -0500

    added methods

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,9 +1,25 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Counter extends Component {
-  render() { 
-    return <h1>Hello World</h1>
+  state = {
+    count: 0,
+  };
+
+  render() {
+    return (
+      <div>
+        <span className={this.state.count === 0 ? "badge-warning m-2" : "badge-primary m-2"}>
+          {this.formatCount()}
+        </span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+      </div>
+    );
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
- 
+
 export default Counter;
