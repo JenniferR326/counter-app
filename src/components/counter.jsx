62b578e0a50347b7835c48120d8f556d39f3a77c
JenniferR326 <jennarafael@gmail.com>
2021-03-10 12:12:45 -0500

    added methods on counter class

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,13 +1,37 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  // constructor() {
+  //   super();
+  //   this.handleIncrement = this.handleIncrement.bind(this);
+  // }
+
   state = {
     count: 0,
+    tags: ['tag1', 'tag2', 'tag3'],
   };
+
+  handleIncrement = () => {
+    console.log("Increment Clicked");
+    this.setState({ count: this.state.count + 1 });
+  }
+
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags</p>;
+    return (
+      <ul>
+        {this.state.tags.map((tag) => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
 
   render() {
     return (
       <div>
+        {this.state.tags.length === 0 && "please create a new tag"}
+        {this.renderTags()}
         <span
           className={
             this.state.count === 0 ? "badge-warning m-2" : "badge-primary m-2"
@@ -15,7 +39,13 @@ class Counter extends Component {
         >
           {this.formatCount()}
         </span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
+
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-lg"
+        >
+          Increment
+        </button>
       </div>
     );
   }
