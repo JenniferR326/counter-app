82f7dce18504e565081eef51a6238ac310c5cc9b
JenniferR326 <jennarafael@gmail.com>
2021-03-09 22:23:44 -0500

    refactored count component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -8,7 +8,11 @@ class Counter extends Component {
   render() {
     return (
       <div>
-        <span className={this.state.count === 0 ? "badge-warning m-2" : "badge-primary m-2"}>
+        <span
+          className={
+            this.state.count === 0 ? "badge-warning m-2" : "badge-primary m-2"
+          }
+        >
           {this.formatCount()}
         </span>
         <button className="btn btn-secondary btn-sm">Increment</button>
